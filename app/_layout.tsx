60a5fbe00df6acf22a4a32e72aaf11dc35127544
Resh <118062429+reshbuilds@users.noreply.github.com>
2025-10-04 21:22:08 +0200

    Remove ChildrenContext reference from app layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,11 @@
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { ChildrenProvider } from '../src/contexts/ChildrenContext';
-import '../global.css';
 
 export default function RootLayout() {
   return (
-    <ChildrenProvider>
+    <>
       <Stack screenOptions={{ headerShown: false }} />
       <StatusBar style="auto" />
-    </ChildrenProvider>
+    </>
   );
 }
